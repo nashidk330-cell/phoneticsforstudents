1d33663305ff317845b37c63a5b2c34ca69e5b2d
nashidk330-cell <nashidk330@gmail.com>
2025-10-25 15:53:16 +0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'phonetics-notebook-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
