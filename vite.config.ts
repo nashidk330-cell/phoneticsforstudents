f4485741cc2eecc8b97cbe2a3efd755498550fb9
nashidk330-cell <nashidk330@gmail.com>
2025-10-25 13:00:01 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace <YOUR_REPOSITORY_NAME> with your actual GitHub repo name
-  base: '/<YOUR_REPOSITORY_NAME>/',
+  base: '/phonetics-for-students/',
 })
